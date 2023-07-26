7b45b889442e6d5751bc74a08b7e70a8edb9e2e1
arBishal <arbishal.319@gmail.com>
2023-07-26 17:15:02 +0600

    Filter tasks

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,31 +1,60 @@
-import {Template} from 'meteor/templating';
-import {TasksCollection} from '../api/TasksCollection';
+import { Template } from 'meteor/templating';
+import { TasksCollection } from '../api/TasksCollection';
+import { ReactiveDict } from 'meteor/reactive-dict';
 import './App.html';
 import './Task.js';
 
+const HIDE_COMPLETED_STRING = 'hideCompleted';
+
+Template.mainContainer.onCreated(function mainContainerOnCreated() {
+  this.state = new ReactiveDict();
+});
+
+Template.mainContainer.events({
+  'click #hide-completed-button'(event, instance) {
+    const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+    instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
+  },
+});
+
 Template.mainContainer.helpers({
-    tasks() {
-        return TasksCollection.find({}, {sort: {createdAt: -1}});
-    },
+  tasks() {
+    const instance = Template.instance();
+    const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
+
+    const hideCompletedFilter = { isChecked: { $ne: true } };
+
+    return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
+      sort: { createdAt: -1 },
+    }).fetch();
+  },
+  hideCompleted() {
+    return Template.instance().state.get(HIDE_COMPLETED_STRING);
+  },
+  incompleteCount() {
+    const incompleteTasksCount = TasksCollection.find({
+      isChecked: { $ne: true },
+    }).count();
+    return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
+  },
 });
 
 Template.form.events({
-    "submit .task-form"(event) {
+  'submit .task-form'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
 
-        // Prevent default browser form submit
-        event.preventDefault();
+    // Get value from form element
+    const { target } = event;
+    const text = target.text.value;
 
-        // Get value from form element
-        const target = event.target;
-        const text = target.text.value;
+    // Insert a task into the collection
+    TasksCollection.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
 
-        // Insert a task into the collection
-        TasksCollection.insert({
-            text,
-            createdAt: new Date(),  // current time
-        });
-
-        // Clear form after submission
-        target.text.value = "";
-    }
+    // Clear form
+    target.text.value = '';
+  },
 });
